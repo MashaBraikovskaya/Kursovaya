26796396c2212facbf0ec8c2ae039c5d2cb9945d
MashaBraikovskaya <braikowskaya.masha@yandex.ru>
2017-05-11 14:54:42 +0400

    init

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,26 @@
+package hello;
+
+/**
+ * Created by 1 on 25.04.2017.
+ */
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
